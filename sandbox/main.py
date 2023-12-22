188037c58623df2a0addbcfadc0af8bfea0a301a
James Middleton <james.ben.middleton@gmail.com>
2023-12-22 12:31:16 +0000

    Gravity working.

--- sandbox/main.py
+++ sandbox/main.py
@@ -2,24 +2,41 @@
 
 import pygame
 import random
+import math
 
-STARTING_BLUE_BLOBS = 10
-STARTING_RED_BLOBS = 3
-
+FPS = 240
 WIDTH = 800
 HEIGHT = 600
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+G = 0.01 # gravitational constant
+dt = 1 # timestep
+r = 2 # ?
 
-game_display = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Orbit Propagation")
-clock = pygame.time.Clock()
+# --- HELPERS ---
 
 class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+def distSquared(a, b):
+  return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)
+
+def sub(a, b):
+  return Vector(a.x - b.x, a.y - b.y)
+
+def add(a, b):
+  return Vector(a.x + b.x, a.y + b.y)
+
+def mult(a, scalar):
+  return Vector(a.x * scalar, a.y * scalar)
+
+# ---------------
+
+game_display = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Orbit Propagation")
+clock = pygame.time.Clock()
 
 class Body:
 
@@ -28,7 +45,7 @@ class Body:
         self.vel = vel
         self.next_pos = pos
         self.next_vel = vel
-        self.size = random.randrange(4,8)
+        self.size = random.randrange(4,50)
 
     def move(self):
         self.pos = self.next_pos
@@ -37,42 +54,52 @@ class Body:
         self.next_pos.x += self.vel.x
         self.next_pos.y += self.vel.y
 
-        # self.next_vel.x =
-        # self.next_vel.y =
-
         if self.pos.x < 0: self.pos.x = 0
         elif self.pos.x > WIDTH: self.pos.x = WIDTH
         
         if self.pos.y < 0: self.pos.y = 0
         elif self.pos.y > HEIGHT: self.pos.y = HEIGHT
 
+def gravityAcc(pos_a, pos_b, mass):
+  dSq = distSquared(pos_a, pos_b);
+  if dSq <= (4 * r * r):
+    return Vector(0, 0)
+  return mult(sub(pos_a, pos_b), dt * G * mass / (dSq * math.sqrt(dSq)))
 
-def draw_environment(blob_list):
-    game_display.fill(WHITE)
+def gravity(bodies):
+    for a in bodies:
+        for b in bodies:
+            if a == b:
+                continue
+            acc = gravityAcc(b.pos, a.pos, a.size)
+            a.next_vel.x += acc.x
+            a.next_vel.y += acc.y
+    return
 
-    for blob_dict in blob_list:
-        for blob_id in blob_dict:
-            blob = blob_dict[blob_id]
-            pygame.draw.circle(game_display, (0,0,0), [blob.pos.x, blob.pos.y], blob.size)
-            blob.move()
-
-    pygame.display.update()
+def update_physics(bodies):
+    gravity(bodies)
+    return
     
-
 def main():
-    bodies = {}
-    for i in range(10):
+    bodies = []
+    for _ in range(10):
         pos = Vector(random.randrange(0,WIDTH), random.randrange(0,HEIGHT))
-        vel = Vector(random.randrange(-3,3), random.randrange(-3,3))
-        bodies[i] = Body(pos, vel)
+        # vel = Vector(random.randrange(-3,3), random.randrange(-3,3))
+        vel = Vector(0, 0)
+        bodies.append(Body(pos, vel))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        draw_environment([bodies])
-        clock.tick(120)
+        game_display.fill(WHITE)
+        for body in bodies:
+            body.move()
+            pygame.draw.circle(game_display, BLACK, [body.pos.x, body.pos.y], body.size)
+        update_physics(bodies)
+        pygame.display.update()
+        clock.tick(FPS)
 
 if __name__ == '__main__':
     main()
