5b712d8fb861b98a5c000a095d314e1dbbb3385c
James Middleton <james.ben.middleton@gmail.com>
2023-12-23 20:38:48 +0000

    Now with tails!

--- sandbox/main.py
+++ sandbox/main.py
@@ -8,8 +8,9 @@
 
 import pygame
 import math
+import copy
 
-FPS = 240
+FPS = 480
 WIDTH = 128
 HEIGHT = 128
 CENTER_X = WIDTH/2
@@ -18,7 +19,8 @@ BLACK = (0,0,0)
 WHITE = (255,255,255)
 G = 0.01 # gravitational constant
 dt = 1 # timestep
-UNIT=64/32
+UNIT = 64/32
+TRAIL_LENGTH = 600
 
 # --- HELPERS ---
 
@@ -48,25 +50,27 @@ clock = pygame.time.Clock()
 class Body:
 
     def __init__(self, pos, vel, radius, mass):
-        self.pos = pos
+        self.pos = [copy.deepcopy(pos)]
         self.vel = vel
-        self.next_pos = pos
+        self.next_pos = copy.deepcopy(pos)
         self.next_vel = vel
         self.radius = radius
         self.mass = mass
 
     def move(self):
-        self.pos = self.next_pos
+        self.pos.insert(0, copy.deepcopy(self.next_pos))
+        self.pos = self.pos[:TRAIL_LENGTH]
         self.vel = self.next_vel
 
         self.next_pos.x += self.vel.x
         self.next_pos.y += self.vel.y
 
-        if self.pos.x < 0: self.vel.x = (-self.vel.x)
-        elif self.pos.x > WIDTH: self.vel.x = (-self.vel.x)
+        if self.next_pos.x < 0: self.vel.x = (-self.vel.x)/2
+        elif self.next_pos.x > WIDTH: self.vel.x = (-self.vel.x)/2
 
-        if self.pos.y < 0: self.vel.y = (-self.vel.y)
-        elif self.pos.y > HEIGHT: self.vel.y = (-self.vel.y)
+        if self.next_pos.y < 0: self.vel.y = (-self.vel.y)/2
+        elif self.next_pos.y > HEIGHT: self.vel.y = (-self.vel.y)/2
+
 
 def gravityAcc(pos_a, pos_b, radius, mass):
   dSq = distSquared(pos_a, pos_b);
@@ -79,7 +83,7 @@ def gravity(bodies):
         for b in bodies:
             if a == b:
                 continue
-            acc = gravityAcc(b.pos, a.pos, a.radius, a.mass)
+            acc = gravityAcc(b.pos[0], a.pos[0], a.radius, a.mass)
             a.next_vel.x += acc.x
             a.next_vel.y += acc.y
     return
@@ -90,27 +94,11 @@ def update_physics(bodies):
     
 def main():
     bodies = []
-    # bodies.append(Body(Vector(CENTER_X,CENTER_Y), Vector(0,0), 5, 332950)) # sun
-    # bodies.append(Body(Vector(CENTER_X+(0.3871*UNIT), CENTER_Y), Vector(0,0.0), 2, 0.055)) # mercury
-    # bodies.append(Body(Vector(CENTER_X+(0.7223*UNIT), CENTER_Y), Vector(0,0.0), 2, 0.815)) # venus
-    # bodies.append(Body(Vector(CENTER_X+(1*UNIT), CENTER_Y), Vector(0,0.0), 2, 1)) # earth
-    # bodies.append(Body(Vector(CENTER_X+(1.52368055*UNIT),CENTER_Y), Vector(0,0.0), 2, 0.107)) # mars
-    # bodies.append(Body(Vector(CENTER_X+(5.2038*UNIT), CENTER_Y), Vector(0,0.0), 2, 317.8)) # jupiter
-    # bodies.append(Body(Vector(CENTER_X+(9.5826*UNIT), CENTER_Y), Vector(0,0.0), 2, 95.159)) # saturn
-    # bodies.append(Body(Vector(CENTER_X+(20.0965*UNIT), CENTER_Y), Vector(0,0.0), 2, 14.536)) # uranus
-    # bodies.append(Body(Vector(CENTER_X+(30.33*UNIT), CENTER_Y), Vector(0,0.0), 2, 17.147)) # neptune
-
-    # earth-moon-s/c system
-    # bodies.append(Body(Vector(CENTER_X,CENTER_Y), Vector(0,0), 8, 332950)) # earth
-    # bodies.append(Body(Vector(CENTER_X+44, CENTER_Y), Vector(0,0.0585), 3, 14.536)) # moon
-    # bodies.append(Body(Vector(CENTER_X+10, CENTER_Y), Vector(0,0.035), 1, 0.7)) # s/c
-
     
     # chaotic 3 body system
-    bodies.append(Body(Vector(CENTER_X,CENTER_Y+21), Vector(0,0.1), 5, 50)) # earth
-    bodies.append(Body(Vector(CENTER_X+24,CENTER_Y-10), Vector(0,-0.1), 5, 50)) # earth
-    bodies.append(Body(Vector(CENTER_X-23,CENTER_Y-10), Vector(0,0), 5, 50)) # earth
-
+    bodies.append(Body(Vector(CENTER_X,CENTER_Y+21), Vector(0,0.1), 5, 25)) 
+    bodies.append(Body(Vector(CENTER_X+24,CENTER_Y-10), Vector(0,-0.1), 5, 25)) 
+    bodies.append(Body(Vector(CENTER_X-23,CENTER_Y-10), Vector(0,0), 5, 25))
 
     while True:
         for event in pygame.event.get():
@@ -120,8 +108,10 @@ def main():
         game_display.fill(BLACK)
         for body in bodies:
             body.move()
-            pygame.draw.circle(game_display, WHITE, [body.pos.x, body.pos.y], body.radius)
-        pygame.draw.circle(game_display, WHITE, [bodies[0].pos.x, bodies[0].pos.y], bodies[0].radius)
+            for i, trail in enumerate(reversed(body.pos)):
+                radius=(body.radius/TRAIL_LENGTH)*i
+                shade = (255/TRAIL_LENGTH)*i
+                pygame.draw.circle(game_display, (shade, shade, shade), [trail.x, trail.y], radius)
         update_physics(bodies)
         pygame.display.update()
         clock.tick(FPS)
